fc3eeffdbe4dfddab16da7c7de870ec90c915df6
kirubel demelash <62418642+kiztopia1@users.noreply.github.com>
2023-03-10 06:43:09 +0300

    hero created

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./hero.css";
+export default function Hero() {
+  return (
+    <div className="hero">
+      <img
+        src="https://www.thereporterethiopia.com/wp-content/uploads/2022/07/I-ALWAYS-SEE.jpg"
+        alt=""
+      />
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Nav from "../components/nav";
 import Events from "../components/Events";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
     <div>
       <Nav></Nav>
+      <Hero />
       <Events title={"Events in Online"} />
       <Events title={"Events in Addis "} />
     </div>
